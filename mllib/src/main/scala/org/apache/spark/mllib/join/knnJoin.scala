cb31358e8edecae11ca7876c87eaa56fb9c2625d
Sasha Shanina <alekshanina@gmail.com>
2015-06-29 13:12:51 -0400

    fixed comment style

--- mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
@@ -4,9 +4,10 @@ object knnJoin
 	def main (args: Array[String])
 	{
 
-		/* the input: <HDFS input file R points> <HDFS input file S points> <num. of partitions> <HDFS outfile file>
-		 * outer: R points; inner: S points 
-		 */
+		/**
+                  * the input: <HDFS input file R points> <HDFS input file S points> <num. of partitions> <HDFS outfile file>
+		  * outer: R points; inner: S points 
+		  */
 
 		val outer = sc.textFile(args[0])
 		val inner = sc.textFile(args[1])
