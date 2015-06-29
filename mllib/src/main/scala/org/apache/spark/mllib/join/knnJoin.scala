89d866ba8bc717a9399a300ef8640ee6c0745637
Sasha Shanina <alekshanina@gmail.com>
2015-06-29 12:32:48 -0400

    short code to read in the file names

---
+++ mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
@@ -0,0 +1,18 @@
+object knnJoin
+{
+
+	def main (args: Array[String])
+	{
+
+		/* the input: <HDFS input file R points> <HDFS input file S points> <num. of partitions> <HDFS outfile file>
+		 * outer: R points; inner: S points 
+		 */
+
+		val outer = sc.textFile(args[0])
+		val inner = sc.textFile(args[1])
+		val n = args[2]
+
+		outer.count()
+		inner.count()
+	}
+}
