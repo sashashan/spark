d361d82350e0c27781520365be8896c133b04c03
Sasha <sashashan@users.noreply.github.com>
2015-07-01 15:37:48 -0400

    testing sparkcontext

--- mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
@@ -1,1 +1,23 @@
 #hi
+
+package org.apache.spark.mllib.join
+import org.sparkalgos.mllib.join.KnnJoin
+
+object knnJoin {
+	
+
+	def main (args: Array[String]) {
+		/**
+      * the input: <HDFS input file R points> <HDFS input file S points> <num. of partitions> <HDFS outfile file>
+      * outer: R points; inner: S points 
+      */
+		
+		println("Hello World!")
+		
+		val sc = new SparkContext("local","knn")
+
+		val outer = sc.textFile(args(0))
+		outer.count()
+	}
+
+}
