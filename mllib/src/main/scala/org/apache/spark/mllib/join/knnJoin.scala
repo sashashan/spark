b584e40eecca45bb9b705717c564e3c8926e9c02
Sasha <sashashan@users.noreply.github.com>
2015-07-06 17:00:59 -0400

    Update knnJoin.scala

--- mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext._ 
+import org.apache.spark.rdd.RDD
+import scala.collection.immutable.Vector
 import java.util.Random
 
 object knnJoin {
