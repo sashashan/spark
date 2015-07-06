4aac73d86879a2c6ae4d519503772f4bdf988793
Sasha <sashashan@users.noreply.github.com>
2015-07-06 12:53:09 -0400

    updated arguments on run

--- mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
@@ -1,22 +1,31 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext._ 
+import java.util.Random
 
 object knnJoin {
 	
+	/**
+	 * Top-level methods for calling knnJoin clustering.
+	 * @param r_points points of the R set 
+	 * @param s_points points of the S set
+	 * @param dim the dimenstion of data sets
+	 * @param k number of nearest neighbours 
+	 * @param n number of partitions
+	 */
 
-	def main (args: Array[String]) {
-		/**
-      * the input: <HDFS input file R points> <HDFS input file S points> <num. of partitions> <HDFS outfile file>
-      * outer: R points; inner: S points 
-      */
+	def run(
+		r_points: RDD[String]
+		s_points: RDD[String]
+		dim: Int
+		k: Int
+		n: Int)
 		
 		println("Hello World!")
 		
-                val sc = new SparkContext("yarn-cluster","knn")
-
-		val outer = sc.textFile(args(0))
-		outer.count()
+		val rand = new Random(n)
+		
+		
 	}
 
 }
