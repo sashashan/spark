199ac48526453151ac81a482af966c09cec21095
Sasha <sashashan@users.noreply.github.com>
2015-07-09 13:59:59 -0400

    Update knnJoin.scala

--- mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
@@ -1,21 +1,36 @@
-package org.apache.spark.mllib.join
-import org.sparkalgos.mllib.join.KnnJoin
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext._ 
+import org.apache.spark.rdd.RDD
+import scala.collection.immutable.Vector
+import java.util.Random
+
 
 object knnJoin {
 	
+	/**
+	 * Top-level methods for calling knnJoin clustering.
+	 * @param r_points points of the R set 
+	 * @param s_points points of the S set
+	 * @param dim the dimenstion of data sets
+	 * @param k number of nearest neighbours 
+	 * @param n number of partitions
+	 */
 
-	def main (args: Array[String]) {
-		/**
-                  * the input: <HDFS input file R points> <HDFS input file S points> <num. of partitions> <HDFS outfile file>
-                  * outer: R points; inner: S points 
-                  */
+	def run(
+		r_points: RDD[Vector[Double]],
+		s_points: RDD[Vector[Double]],
+		dim: Int,
+		k: Int,
+		n: Int){
 		
 		println("Hello World!")
 		
-		val sc = new SparkContext("local","knn")
-
-		val outer = sc.textFile(args(0))
-		outer.count()
+		val rand = new Random(n)
+		
+		//vectors.map(e => (rand.nextInt(5), e))
+		
+		
 	}
 
 }
