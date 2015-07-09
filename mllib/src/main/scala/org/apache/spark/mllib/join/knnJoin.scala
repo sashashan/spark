54f4b5971ef8adc4c0aca64a55ab0c98a47eed2a
Sasha <sashashan@users.noreply.github.com>
2015-07-09 15:01:39 -0400

    Update knnJoin.scala

--- mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
@@ -6,20 +6,20 @@ import scala.collection.immutable.Vector
 import java.util.Random
 
 //Testing elki library
-import de.lmu.ifi.dbs.elki.data.FloatVector;
-import de.lmu.ifi.dbs.elki.data.DoubleVector;
-import de.lmu.ifi.dbs.elki.database.DistanceResultPair;
-import de.lmu.ifi.dbs.elki.distance.FloatDistance;
-import de.lmu.ifi.dbs.elki.distance.DoubleDistance;
-import de.lmu.ifi.dbs.elki.distance.distancefunction.DistanceFunction;
-import de.lmu.ifi.dbs.elki.distance.distancefunction.EuclideanDistanceFunction;
-import de.lmu.ifi.dbs.elki.index.tree.spatial.rstarvariants.rstar.RStarTree;
-import de.lmu.ifi.dbs.elki.index.tree.spatial.*;
-import de.lmu.ifi.dbs.elki.index.tree.TreeIndex;
-import de.lmu.ifi.dbs.elki.index.*;
-import de.lmu.ifi.dbs.elki.parser.*;
-import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException;
-import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization;
+import de.lmu.ifi.dbs.elki.data.FloatVector
+import de.lmu.ifi.dbs.elki.data.DoubleVector
+import de.lmu.ifi.dbs.elki.database.DistanceResultPair
+import de.lmu.ifi.dbs.elki.distance.FloatDistance
+import de.lmu.ifi.dbs.elki.distance.DoubleDistance
+import de.lmu.ifi.dbs.elki.distance.distancefunction.DistanceFunction
+import de.lmu.ifi.dbs.elki.distance.distancefunction.EuclideanDistanceFunction
+import de.lmu.ifi.dbs.elki.index.tree.spatial.rstarvariants.rstar.RStarTree
+import de.lmu.ifi.dbs.elki.index.tree.spatial._
+import de.lmu.ifi.dbs.elki.index.tree.TreeIndex
+import de.lmu.ifi.dbs.elki.index._
+import de.lmu.ifi.dbs.elki.parser._
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.ParameterException
+import de.lmu.ifi.dbs.elki.utilities.optionhandling.parameterization.ListParameterization
 
 object knnJoin {
 	
