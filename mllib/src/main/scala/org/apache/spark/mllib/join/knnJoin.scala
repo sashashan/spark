1564ae8e324435ee5a4cd2779582bce7e94138e7
Sasha <sashashan@users.noreply.github.com>
2015-07-14 17:42:47 -0400

    Update knnJoin.scala

--- mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/join/knnJoin.scala
@@ -83,6 +83,7 @@ object knnJoin {
       		println(zval)
 		//val b = new B(zval.asInstanceOf[java.io.Serializable], 0) //source
 		val hi = "hi".asInstanceOf[java.io.Serializable]
+		hi
 		val b = new B(hi, hi) //source
 		println(zval.toString)
 		println("from func1")
