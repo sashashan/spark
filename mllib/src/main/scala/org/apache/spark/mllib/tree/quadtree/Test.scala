8a3eb0e8a5cebddd84cd5c6313e241414d9d9fd1
Sasha <sashashan@users.noreply.github.com>
2015-07-20 10:37:34 -0400

    Update Test.scala

--- mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
@@ -1,28 +1,30 @@
+import org.apache.spark.rdd.RDD
 import java.awt.Rectangle
 import java.util.ArrayList
 
 object Test{
 
   def main(
-    r_points: RDD[String],
-		s_points: RDD[String]) {
+    	r_points: RDD[String],
+	s_points: RDD[String]) {
 		  
     val height = 2
     val width = 2
     
     val quad = new Quadtree(0, new Rectangle(0, 0, 600, 600))
     quad.clear()
-    val allObjects = new ArrayList() // S set
+    val allObjects = new ArrayList[Rectangle]() // S set
     for (i <- 0 until allObjects.size) {
       quad.insert(allObjects.get(i))
     }
-    val rObjects = new ArrayList() // R set
-    val returnObjects = new ArrayList()
+    val rObjects = new ArrayList[Rectangle]() // R set
+    val returnObjects = new ArrayList[Rectangle]()
     for (i <- 0 until allObjects.size) {
       returnObjects.clear()
       quad.retrieve(returnObjects, rObjects.get(i))
-      for (x <- 0 until returnObjects.size) {
-        println(returnObjects.get(x).x)
+      for (l <- 0 until returnObjects.size) {
+        println("X val" + returnObjects.get(l).x)
+        println("Y val" + returnObjects.get(l).y)
       }
     }
   }
