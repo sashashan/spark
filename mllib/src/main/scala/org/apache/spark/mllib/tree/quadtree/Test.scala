63972ee91db3f13a83c151f223f716f3a7669bfa
Sasha <sashashan@users.noreply.github.com>
2015-07-20 00:50:28 -0400

    Update Test.scala

--- mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
@@ -2,5 +2,17 @@ object Test{
 
   def main(args: Array[String]) {
     val quad = new Quadtree(0, new Rectangle(0, 0, 600, 600))
+    quad.clear()
+    val allObjects = new ArrayList()
+    for (i <- 0 until allObjects.size) {
+      quad.insert(allObjects.get(i))
+    }
+    val returnObjects = new ArrayList()
+    for (i <- 0 until allObjects.size) {
+      returnObjects.clear()
+      quad.retrieve(returnObjects, objects.get(i))
+      for (x <- 0 until returnObjects.size) {
+      }
+    }
   }
 }
