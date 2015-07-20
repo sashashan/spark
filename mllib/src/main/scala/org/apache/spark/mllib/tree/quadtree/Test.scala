e2c1f96d9c1e550eb55cf12fbbea57f728e2f6da
Sasha <sashashan@users.noreply.github.com>
2015-07-20 10:18:34 -0400

    Update Test.scala

--- mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
@@ -3,18 +3,26 @@ import java.util.ArrayList
 
 object Test{
 
-  def main(args: Array[String]) {
+  def main(
+    r_points: RDD[String],
+		s_points: RDD[String]) {
+		  
+    val height = 2
+    val width = 2
+    
     val quad = new Quadtree(0, new Rectangle(0, 0, 600, 600))
     quad.clear()
-    val allObjects = new ArrayList()
+    val allObjects = new ArrayList() // S set
     for (i <- 0 until allObjects.size) {
       quad.insert(allObjects.get(i))
     }
+    val rObjects = new ArrayList() // R set
     val returnObjects = new ArrayList()
     for (i <- 0 until allObjects.size) {
       returnObjects.clear()
-      quad.retrieve(returnObjects, objects.get(i))
+      quad.retrieve(returnObjects, rObjects.get(i))
       for (x <- 0 until returnObjects.size) {
+        println(returnObjects.get(x).x)
       }
     }
   }
