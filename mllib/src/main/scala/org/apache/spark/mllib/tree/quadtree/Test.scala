3518f1c0b515c12b1abec37560f3abe04ab0b49d
Sasha <sashashan@users.noreply.github.com>
2015-07-22 12:10:14 -0400

    Update Test.scala

--- mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
@@ -24,7 +24,7 @@ object Test{
     val rObjects = new ArrayList[Point]() // R set
     recData2.collect().foreach(line => rObjects.add(line))
     
-    val quad = new Quadtree(0, new Rectangle(0, 0, 12, 12))
+    val quad = new Quadtree(0, new Rectangle(0, 0, 10, 10))
     quad.clear()
     
     for (i <- 0 until allObjects.size) {
