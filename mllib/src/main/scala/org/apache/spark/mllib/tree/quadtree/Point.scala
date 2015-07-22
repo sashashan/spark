6f1ecf74c54e7cb1a03952cd7c9719576de4326f
Sasha <sashashan@users.noreply.github.com>
2015-07-22 11:06:44 -0400

    Update Point.scala

--- mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Point.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Point.scala
@@ -1,6 +1,6 @@
 package org.apache.spark.mllib.tree.quadtree
 
-class Point(var x: Double, var y: Double) extends Serializable {
+class Point(var x: Int, var y: Int) extends Serializable {
   
   def getX = x
   def getY = y
