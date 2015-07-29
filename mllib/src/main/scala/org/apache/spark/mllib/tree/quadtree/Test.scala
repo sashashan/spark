d7e258238d19de0aea75839e75ac99d307e82836
Sasha <sashashan@users.noreply.github.com>
2015-07-29 11:34:43 -0400

    Update Test.scala

--- mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Test.scala
@@ -1,8 +1,8 @@
 package org.apache.spark.mllib.tree.quadtree
 
 import org.apache.spark.rdd.RDD
-import java.awt.Rectangle
 import java.util.ArrayList
+import java.awt.geom.Rectangle2D
 
 object Test{
 
@@ -14,28 +14,30 @@ object Test{
     val width = 1
     
     val parsedData = s_points.map(_.split(' ')) //RDD[String]
-    val recData = parsedData.map(line => new Rectangle(line(0).toInt, line(1).toInt, width, height))
-    val allObjects = new ArrayList[Rectangle]() // S set
+    val recData = parsedData.map(line => new Point(line(0).toInt, line(1).toInt))
+    val allObjects = new ArrayList[Point]() // S set
     recData.collect().foreach(line => allObjects.add(line))
     
     
     val parsedData2 = r_points.map(_.split(' '))
-    val recData2 = parsedData.map(line => new Rectangle(line(0).toInt, line(1).toInt, width, height))
-    val rObjects = new ArrayList[Rectangle]() // R set
+    val recData2 = parsedData2.map(line => new Point(line(0).toInt, line(1).toInt))
+    val rObjects = new ArrayList[Point]() // R set
     recData2.collect().foreach(line => rObjects.add(line))
     
-    val quad = new Quadtree(0, new Rectangle(0, 0, 12, 12))
+    val quad = new Quadtree(0, new Rectangle2D.Double(0, 0, 10, 10))
     quad.clear()
     
     for (i <- 0 until allObjects.size) {
       quad.insert(allObjects.get(i))
     }
     
-    val returnObjects = new ArrayList[Rectangle]()
-    for (i <- 0 until allObjects.size) {
+    quad.printTree
+    
+    val returnObjects = new ArrayList[Point]()
+    for (i <- 0 until rObjects.size) {
       returnObjects.clear()
-      println("## For obj " + allObjects.get(i).toString())
-      quad.retrieve(returnObjects, rObjects.get(i))
+      println("## For obj " + rObjects.get(i).toString())
+      quad.retrieveForKNN(returnObjects, rObjects.get(i))
       for (l <- 0 until returnObjects.size) {
         println(returnObjects.get(l).toString())
       }
