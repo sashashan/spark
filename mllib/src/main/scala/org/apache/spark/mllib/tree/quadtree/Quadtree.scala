5ccf981c58c14c54021d1cd7688707d5041d9f04
Sasha <sashashan@users.noreply.github.com>
2015-07-29 11:34:04 -0400

    Update Quadtree.scala

--- mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Quadtree.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/tree/quadtree/Quadtree.scala
@@ -1,16 +1,20 @@
 package org.apache.spark.mllib.tree.quadtree
+import scala.util.Sorting
 
 import scala.collection.JavaConversions._
-import java.awt.Rectangle
 import java.util.ArrayList
+import java.awt.geom.Rectangle2D
 
-class Quadtree(private var level: Int, private var bounds: Rectangle) {
+class Quadtree(private var level: Int, private var bounds: Rectangle2D) {
   // bounds represents the 2D space that the node occupies
-  private var MAX_LEVELS: Int = 5
-  private var MAX_OBJECTS: Int = 10
-  private var objects = new ArrayList[Rectangle]()
+  private var MAX_LEVELS: Int = 1000 
+  private var MAX_OBJECTS: Int = 3 // maximum objects per node
+  private var objects = new ArrayList[Point]()
   private var nodes: Array[Quadtree] = new Array[Quadtree](4)
   
+  /**
+   * Clears the quadtree.
+   */
   def clear() {
     objects.clear()
     for (i <- 0 until nodes.length if nodes(i) != null) {
@@ -19,30 +23,44 @@ class Quadtree(private var level: Int, private var bounds: Rectangle) {
     }
   }
   
+  /**
+   * Splits the node is split into 4.
+   */
   private def split() {
-    val subWidth = (bounds.getWidth / 2).toInt
-    val subHeight = (bounds.getHeight / 2).toInt
-    val x = bounds.getX.toInt
-    val y = bounds.getY.toInt
-    nodes(0) = new Quadtree(level + 1, new Rectangle(x + subWidth, y, subWidth, subHeight))
-    nodes(1) = new Quadtree(level + 1, new Rectangle(x, y, subWidth, subHeight))
-    nodes(2) = new Quadtree(level + 1, new Rectangle(x, y + subHeight, subWidth, subHeight))
-    nodes(3) = new Quadtree(level + 1, new Rectangle(x + subWidth, y + subHeight, subWidth, subHeight))
+    val subWidth = (bounds.getWidth / 2)
+    val subHeight = (bounds.getHeight / 2)
+    val x = bounds.getX
+    val y = bounds.getY
+    nodes(0) = new Quadtree(level + 1, new Rectangle2D.Double(x + subWidth, y, subWidth, subHeight))
+    nodes(1) = new Quadtree(level + 1, new Rectangle2D.Double(x, y, subWidth, subHeight))
+    nodes(2) = new Quadtree(level + 1, new Rectangle2D.Double(x, y + subHeight, subWidth, subHeight))
+    nodes(3) = new Quadtree(level + 1, new Rectangle2D.Double(x + subWidth, y + subHeight, subWidth, subHeight))
   }
 
-  private def getIndex(pRect: Rectangle): Int = {
+  /**
+   * Determines which node the object belongs to.
+   * -1 means that it is on the midline and so cannot be passed to children;
+   * and it is left inside the parent node.
+   *  _________
+   * | 1  | 0  |
+   * |____|____|
+   * | 2  | 3  | 
+   * |____|____|
+   */
+  private def getIndex(p: Point): Int = {
     var index = -1
     val verticalMidpoint = bounds.getX + (bounds.getWidth / 2)
     val horizontalMidpoint = bounds.getY + (bounds.getHeight / 2)
-    val topQuadrant = (pRect.getY < horizontalMidpoint && pRect.getY + pRect.getHeight < horizontalMidpoint)
-    val bottomQuadrant = (pRect.getY > horizontalMidpoint)
-    if (pRect.getX < verticalMidpoint && pRect.getX + pRect.getWidth < verticalMidpoint) {
+    // 
+    val topQuadrant = (p.getY < horizontalMidpoint)
+    val bottomQuadrant = (p.getY > horizontalMidpoint)
+    if (p.getX < verticalMidpoint) {
       if (topQuadrant) {
         index = 1
       } else if (bottomQuadrant) {
         index = 2
       }
-    } else if (pRect.getX > verticalMidpoint) {
+    } else if (p.getX > verticalMidpoint) {
       if (topQuadrant) {
         index = 0
       } else if (bottomQuadrant) {
@@ -51,16 +69,33 @@ class Quadtree(private var level: Int, private var bounds: Rectangle) {
     }
     index
   }
+  
+  /**
+   * Checks if the point is inside or touching the node.
+   * This is neccessary for the special case of retrieving the points for kNN 
+   * when the r point lies on the midline. 
+   */
+  def isOutsideTheNode(p: Point): Boolean = {
+    val isAboveOrBelow = (p.getY < bounds.getY) || (p.getY > (bounds.getY + bounds.getHeight))
+    val isRightOrLeft = (p.getX > (bounds.getX + bounds.getWidth)) || (p.getX < bounds.getX)
+    isAboveOrBelow || isRightOrLeft
+  }
 
-  def insert(pRect: Rectangle) {
+  /**
+   * Inserts objects into the tree. When a node exceeds the max capacity
+   * of objects, the node is split into 4 and all of the objects are "moved"
+   * to the coresponding children.
+   */
+  def insert(p: Point) {
+    // if the node is not a leaf ...
     if (nodes(0) != null) {
-      val index = getIndex(pRect)
+      val index = getIndex(p)
       if (index != -1) {
-        nodes(index).insert(pRect)
+        nodes(index).insert(p)
         return
       }
     }
-    objects.add(pRect)
+    objects.add(p)
     if (objects.size > MAX_OBJECTS && level < MAX_LEVELS) {
       if (nodes(0) == null) {
         split()
@@ -77,14 +112,91 @@ class Quadtree(private var level: Int, private var bounds: Rectangle) {
     }
   }
   
-  def retrieve(returnObjects: ArrayList[Rectangle], pRect: Rectangle): ArrayList[Rectangle] = {
-    val index = getIndex(pRect)
-    if (index != -1 && nodes(0) != null) {
-      nodes(index).retrieve(returnObjects, pRect)
+  /**
+   * If the point lies on the midline, it the retrieve method is called for
+   * all the children, and those children that do not contain or touch the point
+   * are discarded.
+   */
+  def retrieveForKNN(returnObjects: ArrayList[Point], p: Point): ArrayList[Point] = {
+    val index = getIndex(p)
+    // Making sure the point is inside the square 
+    if (!isOutsideTheNode(p)) {
+      if (index != -1 && nodes(0) != null) {
+        nodes(index).retrieveForKNN(returnObjects, p)
+      } 
+      // For points that lie on the midline
+      else if (index == -1 && nodes(0) != null) {
+        for (i <- 0 until 4) {
+          nodes(i).retrieveForKNN(returnObjects, p)
+        }
+      }
+      returnObjects.addAll(objects)
     }
-    returnObjects.addAll(objects)
     returnObjects
   }
   
+  def printTree {
+    // printing any surface points
+    if (objects != null) {
+      println("Level: " + level)
+      println("Borders: x: " + bounds.getX + " y: " + bounds.getY + " width: " + bounds.getWidth + " height: " + bounds.getHeight)
+      for (i <- 0 until objects.size) {
+        println(objects.get(i).toString())
+      }
+    }
+    // printing surface points of the children
+    if (nodes(0) != null) {
+      println("Level: " + level)
+      println("From top rght corner")
+      nodes(0).printTree
+      println("Level: " + level)
+      println("From top left corner")
+      nodes(1).printTree
+      println("Level: " + level)
+      println("From bottom left corner")
+      nodes(2).printTree
+      println("Level: " + level)
+      println("From bottom right corner")
+      nodes(3).printTree
+    }
+    else return
+  }
+  
+  def eucledianDist(p1: Point, p2: Point): Int = {
+    var ans = math.sqrt(math.pow(p1.getX - p2.getX, 2) + math.pow(p1.getY - p2.getY, 2)).toInt
+    ans
+  }
+  
+  /**
+   * Finding the k nearest neighbours of point rp. 
+   */
+  def kNN(rp: Point, k: Int, result: ArrayList[Point]): ArrayList[Point] = {
+    val index = getIndex(rp)
+    if (index >= 0) {
+      retrieveForKNN(result, rp)
+      println("Possible points:")
+      for (l <- 0 until result.size) {
+        println(result.get(l).toString())
+      }
+      // Get the distances betweeen the points, sort them and get the first k points
+      var list:Array[Point] = new Array[Point](result.size)
+      for (i <- 0 until result.size) {
+        result.get(i).setDist(eucledianDist(rp, result.get(i)))
+        println("The distance was set to: " + result.get(i).getDist)
+        list(i) = result.get(i)
+      }
+      Sorting.quickSort[Point](list)(DistanceOrdering)
+      println("The List of ordered distances:")
+      for (x <- list ) {
+         println(x)
+      }
+      result.clear()
+      for (i <- 0 until k) {
+        result.add(list(i))
+      }
+    }
+    result
+  }
   
 } //end class
+
